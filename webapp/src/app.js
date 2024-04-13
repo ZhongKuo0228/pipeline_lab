04c5d63cb066f781ba73e5d2884cc07770a1a420
ZhongKuo0228 <rt019623@gmail.com>
2024-04-13 17:19:48 +0800

    pipeline for build image

--- webapp/src/app.js
+++ webapp/src/app.js
@@ -24,7 +24,7 @@ fastify.get('/addTwoNumbers/:num1/:num2', async (request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000, host: '0.0.0.0' })
     console.log(`Server listening on ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err)
